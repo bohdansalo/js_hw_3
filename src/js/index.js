b33102a256e4b55bbc30e52365441f6a2437d1c5
Богдан <bohdansalo31@gmail.com>
2025-01-17 18:25:33 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// const result = 5 + 5 + "5";
+// console.log(`result: ${result}.Тип змінной:${typeof result}`);
+
+const email = "bodya@bodya.com";
+
+if (email.includes("@")) {
+  console.log("Email містить символ '@'");
+} else {
+  console.log("Email не містить символ '@'");
+}
+
+const length = email.length;
+console.log(`Загальна кількість символів в email: ${length}`);
