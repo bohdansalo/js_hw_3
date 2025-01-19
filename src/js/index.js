8c132fb0b08019b4aae315c2a7ddcd3c1e707816
Богдан <bohdansalo31@gmail.com>
2025-01-19 19:16:16 +0200

    completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,28 @@
+//завдання 1
 // const result = 5 + 5 + "5";
 // console.log(`result: ${result}.Тип змінной:${typeof result}`);
 
-const email = "bodya@bodya.com";
+//завдання 2
 
-if (email.includes("@")) {
-  console.log("Email містить символ '@'");
-} else {
-  console.log("Email не містить символ '@'");
-}
+// const email = "bodya@bodya.com";
 
-const length = email.length;
-console.log(`Загальна кількість символів в email: ${length}`);
+// if (email.includes("@")) {
+//   console.log("Email містить символ '@'");
+// } else {
+//   console.log("Email не містить символ '@'");
+// }
+
+// const length = email.length;
+// console.log(`Загальна кількість символів в email: ${length}`);
+//завдання 3
+// const nameMy = "My";
+// const nameS = "name";
+// const nameIs = "is";
+// const fullName = `${nameMy} ${nameS} ${nameIs} Viktor`;
+// console.log(fullName);
+
+//завдання 3
+
+const userName = "Олександра";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
